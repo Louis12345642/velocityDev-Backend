1fcb64b4767eed5a59aa6b0427cf1cd4a3455793
Mubarak kual louis <mubaraklouis@gmail.com>
2024-04-11 22:54:42 +0200

    refactor the newsletter functionality

--- Express.js/src/app.ts
+++ Express.js/src/app.ts
@@ -34,20 +34,20 @@ app.use(express.json());
 /*
 *Handle all the contact routes
 */
-app.use('/contact',contactRouter)
-app.use('/contacts',contactRouter)
-app.use('/contacts/:id',contactRouter)
-app.use('/contacts/:id',contactRouter)
+app.use('/contact', contactRouter)
+app.use('/contacts', contactRouter)
+app.use('/contacts/:id', contactRouter)
+app.use('/contacts/:id', contactRouter)
 
 
 /*
 *Handle all the testimonial routes
 */
 
-app.use('/testimonial',testimonialRouter);
-app.use('/testimonial/index',testimonialRouter);
-app.use('/testimonial/:id',testimonialRouter);
-app.use('/testimonial/:id',testimonialRouter);
+app.use('/testimonial', testimonialRouter);
+app.use('/testimonial/index', testimonialRouter);
+app.use('/testimonial/:id', testimonialRouter);
+app.use('/testimonial/:id', testimonialRouter);
 
 
 
@@ -55,64 +55,102 @@ app.use('/testimonial/:id',testimonialRouter);
 *Handle all the service routes
 */
 
-app.use('/service',serviceRouter);
-app.use('/services/index',serviceRouter);
-app.use('/service/:id',serviceRouter);
-app.use('/service/:id',serviceRouter);
-app.use('/service/:id',serviceRouter)
+app.use('/service', serviceRouter);
+app.use('/services/index', serviceRouter);
+app.use('/service/:id', serviceRouter);
+app.use('/service/:id', serviceRouter);
+app.use('/service/:id', serviceRouter)
 
 
 
 
 //temp code working on the subcription functionality
 
-app.post('/subscribe',(req:any,res:any)=>{
+app.post('/subscribe', (req: any, res: any) => {
 
 
 
-const mailchimp = require("@mailchimp/mailchimp_marketing");
+  const mailchimp = require("@mailchimp/mailchimp_marketing");
 
-mailchimp.setConfig({
-  apiKey: "b435b741c58ba5f0fbd1ce0553d08489-us17",
-  server: "us17",
-});
+  let errors:any = [];
 
-//sub info
+  //initialize mailchimp
 
-const subscriberEmail =req.body.email
 
-const subscribingUser = {
-  firstName: req.body.firstName,
-  lastName: req.body.lastName,
-  email: subscriberEmail
-};
-
-async function run() {
-
-  const list ="c4fb834966"
-
-  const response = await mailchimp.lists.addListMember(list,{
-    email_address: subscribingUser.email,
-    status: "subscribed",
-    merge_fields: {
-      FNAME: subscribingUser.firstName,
-      LNAME: subscribingUser.lastName
-    }
+  mailchimp.setConfig({
+    apiKey: "4dd82828d8da6b3d8f048047ec6289da-us17",
+    server: "us17",
   });
-}
-
-run();
 
 
-const responseData ={
-  "message":"Successfully subscribed to our newsletter",
-  "status":200
-}
+  //sub info
 
-return res.send(responseData)
+  const subscriberEmail = req.body.email
+
+
+  //validate the request
+
+  if (!subscriberEmail) {
+    errors.push({
+      message: "Email is required"
+    });
+  }
+
+  const subscribingUser = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: subscriberEmail
+  };
+
+
+  async function run() {
+
+
+
+
+    const list = "c4fb834966"
+
+
+      const response = await mailchimp.lists.addListMember(list, {
+        email_address: subscribingUser.email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: subscribingUser.firstName,
+          LNAME: subscribingUser.lastName
+
+        }
+      });
+
+
+  }
+
+
+  try{
+
+    
+    const responseData = {
+      "message": "Successfully subscribed to our newsletter",
+      "status": 200
+    }
+
+
+    run();
+    return res.send(responseData);
+  }
+  catch(err){
+
+    errors.push(
+      {
+        "message": " server Error subscribing to newsletter"
+      }
+    )
+
+    if(errors.length > 0)
+    return res.send(errors)
+  }
+
 
 })
-
 
 
 
