40caebdf6091c92ccdaf6cd87070806cc64c301d
Mubarak kual louis <mubaraklouis@gmail.com>
2024-04-14 14:12:34 +0200

    refactor send contact email service

--- src/controllers/ContactController.ts
+++ src/controllers/ContactController.ts
@@ -1,5 +1,4 @@
 import { ContactModel } from "../model/ContactMessages";
-import nodemailer from "nodemailer"; 
 
 /*
   this class is where all the crude operation related to contact are set
@@ -22,50 +21,7 @@ export class ContactController {
       "email": req.body.email,
       "name": req.body.name
     });
-
-    console.log(req.body)
     contactMsg.save();
-
-
-
-    const output =`
-    <h1>New email from ${req.body.name}</h1>
-    <h3>Contact Details</h3>
-    <ul>
-    <li>Name: ${req.body.name}</li>
-    </ul>
-    <p>${req.body.message}</p>`
-        //nodemailer code 
-    const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false, // Use `true` for port 465, `false` for all other ports
-      auth: {
-        user: "luismubarak@gmail.com",
-        pass: "zclj zyzl jiyd nuuq",
-      },
-    });
-    
-    // async..await is not allowed in global scope, must use a wrapper
-    async function main() {
-      // send mail with defined transport object
-      const info = await transporter.sendMail({
-        from: `Mubarak Louis 👻 <luismubarak@gmail.com>`, // sender address
-        to: req.body.email, // list of receivers
-        subject: `Hello ${req.body.name}: Your message has been received`, // Subject line
-        text: req.body.message, // plain text body
-        html: `<b>Thank you ${req.body.name} for contacting velocityDev we are so thrilled to work with you</b>`, // html body
-      });
-    
-      console.log("Message sent: %s", info.messageId);
-      // Message sent: <d786aa62-4e0a-070a-47ed-0b0666549519@ethereal.email>
-    }
-
-    main().catch(console.error);
-    
-    
-
-
     return res.json(contactMsg);
   }
   /*
---
+++ src/service/sendContactEmail.ts
@@ -0,0 +1,48 @@
+/*
+* [] Description: This class is contains all the methods for sending contact emails after the user fills the contact me form
+* [] Method: sendEmail()
+* [] @return : void
+* [] Date: 14/0/2024
+*
+* [] Author(s): mubarak kual louis
+*
+*
+*/
+import nodemailer from "nodemailer";
+
+
+
+class sendContactEmail {
+    public static sendEmail(req: any, res: any) {
+
+        //nodemailer code 
+        const transporter = nodemailer.createTransport({
+            host: "smtp.gmail.com",
+            port: 587,
+            secure: false, // Use `true` for port 465, `false` for all other ports
+            auth: {
+                user: "luismubarak@gmail.com",
+                pass: "zclj zyzl jiyd nuuq",
+            },
+        });
+
+        // async..await is not allowed in global scope, must use a wrapper
+        async function main() {
+            // send mail with defined transport object
+            const info = await transporter.sendMail({
+                from: `Mubarak Louis 👻 <luismubarak@gmail.com>`, // sender address
+                to: req.body.email, // list of receivers
+                subject: `Hello ${req.body.name}: Your message has been received`, // Subject line
+                text: req.body.message, // plain text body
+                html: `<b>Thank you ${req.body.name} for contacting velocityDev we are so thrilled to work with you</b>`, // html body
+            });
+
+            console.log("Message sent: %s", info.messageId);
+
+        }
+        //calling the main fucntion
+
+        main().catch(console.error);
+
+    }
+}
