e2cf3dbea77e07982f1cdc072774fd24b5b2e79f
Mubarak kual louis <mubaraklouis@gmail.com>
2024-04-14 13:06:05 +0200

    congration refactored the mailchim service code successfull one milestone completed

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ require("dotenv").config();
 
 import testimonialRouter from "./routes/TestimonialRoute";
 import serviceRouter from "./routes/serviceRoute";
+import serviceProviderRouter from "./routes/serviceProvidersRoute";
 
 
 
@@ -56,89 +57,11 @@ app.use('/service/:id', serviceRouter);
 app.use('/service/:id', serviceRouter)
 
 
+/*
+*Handle all the external apis services routes
+*/
 
-
-//temp code working on the subcription functionality
-
-app.post('/subscribe', (req: any, res: any) => {
-
-
-
-  const mailchimp = require("@mailchimp/mailchimp_marketing");
-
-  let errors:any = [];
-
-  //initialize mailchimp
-
-
-  mailchimp.setConfig({
-    apiKey:process.env.MAIL_CHIMP_API_KEY,
-    server: "us17",
-  });
-
-
-  //sub info
-
-  const subscriberEmail = req.body.email
-
-
-  //validate the request
-
-  if (!subscriberEmail) {
-    errors.push({
-      message: "Email is required"
-    });
-  }
-
-  const subscribingUser = {
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: subscriberEmail
-  };
-
-
-  async function run() {
-    const list_id = process.env.MAIL_CHIMP_API_LIST_ID;
-      const response = await mailchimp.lists.addListMember(list_id, {
-        email_address: subscribingUser.email,
-        status: "subscribed",
-        merge_fields: {
-          FNAME: subscribingUser.firstName,
-          LNAME: subscribingUser.lastName
-
-        }
-      });
-  }
-
-
-  try{
-
-    
-    const responseData = {
-      "message": "Successfully subscribed to our newsletter",
-      "status": 200
-    }
-
-
-    run();
-    return res.send(responseData);
-  }
-  catch(err){
-
-    errors.push(
-      {
-        "message": " server Error subscribing to newsletter"
-      }
-    )
-
-    if(errors.length > 0)
-    return res.send(errors)
-  }
-
-
-})
-
-
+app.use('/subscribe',serviceProviderRouter)
 
 /*
 *Handle running the server using  port 3000 or Heroku assigned port
